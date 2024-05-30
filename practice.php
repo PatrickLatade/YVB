13e4e55f973cafc09d6bd9e3e19f5e3549b57209
Patrick Jacob Latade <144516251+PatrickLatade@users.noreply.github.com>
2024-05-30 21:59:39 +0800

    Add files via upload

---
+++ practice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico.png">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Heading</h1>
+    <p>Paragraph</p>
+    <p>In out world today, technology is very prevalent.</p>
+</body>
+</html>
